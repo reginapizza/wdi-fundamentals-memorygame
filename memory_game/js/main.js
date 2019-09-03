49ce96dd4ba1f8ba52e3eaf31ba767f0f46fc0f3
Regina Scott <reginascott@Reginas-Air.cable.rcn.com>
2019-09-03 19:37:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+const cards = ["Queen", "Queen", "King", "King"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne) 
+console.log("User flipped Queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped King");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert ("You found a match!");
+	} else {
+		alert ("Sorry, try again.");
+	}
+}
