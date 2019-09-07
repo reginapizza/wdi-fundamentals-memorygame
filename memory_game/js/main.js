6fa8c54a0914a769058f962ed3d3f9af4390158f
Regina Scott <reginascott@Reginas-Air.cable.rcn.com>
2019-09-07 17:38:47 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,38 @@
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+	{
+		rank:"Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank:"Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank:"King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank:"King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 const cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  console.log("You found a match!");
+	  alert("You found a match!");
 	} else {
-	  console.log("Sorry, try again.");
+	  alert("Sorry, try again.");
 	}
 }
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
